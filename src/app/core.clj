b8c382296ec20ace225d8c070eb9cf2fac98f66c
PhoenixCampbell <phjcampbell18@gmail.com>
2025-09-03 01:53:55 -0500

    Adding the ability to add entities

--- src/app/core.clj
+++ src/app/core.clj
@@ -72,4 +72,80 @@
     (if (= -1 ts) (reverse res)
         (let [attr (attr-at db ent-id attr-name ts)]
           (recur (conj res {(:ts attr) (:value attr)}) (:prev-ts attr))))))
- 
+
+(defn add-entity [db ent]
+   (let [[fixed-ent next-top-id] (fix-new-entity db ent)
+         layer-with-updated-storage (update-in 
+                            (last (:layers db)) [:storage] write-entity fixed-ent)
+         add-fn (partial add-entity-to-index fixed-ent)
+         new-layer (reduce add-fn layer-with-updated-storage (indexes))]
+    (assoc db :layers (conj (:layers db) new-layer) :top-id next-top-id)))
+
+(defn- next-ts [db] (inc (:curr-time db)))
+
+(defn- update-creation-ts [ent ts-val]
+   (reduce #(assoc-in %1 [:attrs %2 :ts ] ts-val) ent (keys (:attrs ent))))
+
+(defn- next-id [db ent]
+   (let [top-id (:top-id db)
+         ent-id (:id ent)
+         increased-id (inc top-id)]
+         (if (= ent-id :db/no-id-yet)
+             [(keyword (str increased-id)) increased-id]
+             [ent-id top-id])))
+
+(defn- fix-new-entity [db ent]
+   (let [[ent-id next-top-id] (next-id db ent)
+         new-ts               (next-ts db)]
+       [(update-creation-ts (assoc ent :id ent-id) new-ts) next-top-id]))
+
+(defn- add-entity-to-index [ent layer ind-name]
+   (let [ent-id (:id ent)
+         index (ind-name layer)
+         all-attrs  (vals (:attrs ent))
+         relevant-attrs (filter #((usage-pred index) %) all-attrs)
+         add-in-index-fn (fn [ind attr] 
+                                 (update-attr-in-index ind ent-id (:name attr) 
+                                                                  (:value attr) 
+                                                                  :db/add))]
+        (assoc layer ind-name  (reduce add-in-index-fn index relevant-attrs))))
+
+(defn- update-attr-in-index [index ent-id attr-name target-val operation]
+   (let [colled-target-val (collify target-val)
+         update-entry-fn (fn [ind vl] 
+                             (update-entry-in-index 
+                                ind 
+                                ((from-eav index) ent-id attr-name vl) 
+                                operation))]
+     (reduce update-entry-fn index colled-target-val)))
+
+(defn- update-entry-in-index [index path operation]
+   (let [update-path (butlast path)
+         update-value (last path)
+         to-be-updated-set (get-in index update-path #{})]
+     (assoc-in index update-path (conj to-be-updated-set update-value))))
+
+(defn add-entities [db ents-seq] (reduce add-entity db ents-seq))
+
+(defn remove-entity [db ent-id]
+   (let [ent (entity-at db ent-id)
+         layer (remove-back-refs db ent-id (last (:layers db)))
+         no-ref-layer (update-in layer [:VAET] dissoc ent-id)
+         no-ent-layer (assoc no-ref-layer :storage 
+                                   (drop-entity  
+                                          (:storage no-ref-layer) ent))
+         new-layer (reduce (partial remove-entity-from-index ent) 
+                                 no-ent-layer (indexes))]
+     (assoc db :layers (conj  (:layers db) new-layer))))
+
+(defn- remove-back-refs [db e-id layer]
+   (let [reffing-datoms (reffing-to e-id layer)
+         remove-fn (fn[d [e a]] (update-entity db e a e-id :db/remove))
+         clean-db (reduce remove-fn db reffing-datoms)]
+     (last (:layers clean-db))))
+
+(defn- reffing-to [e-id layer]
+   (let [vaet (:VAET layer)]
+         (for [[attr-name reffing-set] (e-id vaet)
+               reffing reffing-set]
+              [reffing attr-name])))
